7fe5dbbea3e1539760113732085abde7001887d4
AmirthaVarshniMohan <105651923+AmirthaVarshniMohan@users.noreply.github.com>
2024-06-11 20:16:10 +0530

    Connected to Mongo

--- server/api/weather.js
+++ server/api/weather.js
@@ -1,10 +1,14 @@
 const axios = require("axios");
 
+const WEATHER = require("../models/Weather");
+
 // Configuring the path to read the environment variable file, .env, to get the weather api key
 require('dotenv').config({path: "./../../../.env"});
 
 const baseUrl = "http://api.openweathermap.org/data/2.5/weather";
+
 class Weather {
+
     /**
      * Gets the weather data based on the zipcode and which temp system to converge to (imperial/metric system)
      *
@@ -13,6 +17,7 @@ class Weather {
      * @return {JSON} The data response from the weather api call.
      */
     getWeatherData = async (zipCode, tempMetric) => {
+
         /**
          * Use get api for "By ZIP code" (https://openweathermap.org/current#zip)
          * - The "us" query stands for "United States
@@ -20,9 +25,51 @@ class Weather {
          * - "units" query can be either imperial (Fahrenheit) or metric (Celsius)
          */
         let url = `${baseUrl}?zip=${zipCode},us&appid=${process.env.WEATHER_KEY}&units=${tempMetric}`;
+
         // Awaitable call to get the information from the weather api and then return the data.
         // TODO: Add error handling for this call
         return (await axios(url)).data;
+    }
+
+    /**
+     * Saves the weather data using the zipcode as the unique identifier
+     * If it already exists, replace, if not, then add.
+     *
+     * @param {number} zipCode The zipcode used to identify the document to upsert
+     * @param {string} data Weather data to save/update
+     * @return {JSON} The data response from the weather api data.
+     */
+    saveWeatherDataToMongo = async (zipCode, data) => {
+        const filter = {
+            zip: zipCode
+        }
+
+        const replace = {
+            ...filter,
+            ...data,
+            data: Date.now()
+        }
+        await this.findOneReplace(filter, replace);
+    }
+
+    /**
+     * Saves Weather data to MongoDb
+     *
+     * @param {number} zipCode The zipcode used as unique identifier to find the document from mongo
+     * @return {JSON} The data response from the mongodb.
+     */
+    getWeatherDataFromMongo = async (zipCode) => {
+        return WEATHER.findOne({zip: zipCode});
+    }
+
+    /**
+     * If a document already exists with the filter, then replace, if not, add.
+     *
+     * @param {{zip: number}} filter The filter is the zipcode used as unique identifier to find the document from mongo
+     * @return {JSON} The data response from the mongodb.
+     */
+    async findOneReplace(filter, replace) {
+        await WEATHER.findOneAndReplace(filter, replace, {new: true, upsert: true});
     }
 }
 
